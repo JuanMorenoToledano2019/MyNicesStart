110f0f951b35bc9953c320a5250d5b36405fbca1
Juan Moreno Toledano <46522867+JuanMorenoToledano2019@users.noreply.github.com>
2020-12-08 13:23:23 +0100

    He añadido el res y anim

--- app/src/main/java/com/dam/mynicesstart/LoginActivityAlco.java
+++ app/src/main/java/com/dam/mynicesstart/LoginActivityAlco.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivityAlco extends AppCompatActivity {
 
-    TextView mSignUpTextview;
+    protected TextView mSignUpTextview;
+    protected Button mLoginButton;
+    protected TextInputLayout mPassword;
 
 
     @Override
@@ -21,7 +26,7 @@ public class LoginActivityAlco extends AppCompatActivity {
 
 
 
-        mSignUpTextview.setOnClickListener(new View.OnClickListener().OnClickListener(){
+       mSignUpTextview.setOnClickListener(new View.OnClickListener().OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(LoginActivityAlco.this, SignUpActivity.class);
