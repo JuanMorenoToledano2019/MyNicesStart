1653c6772fd8334631830fc2a961fcd4a9d0c893
Juan Moreno Toledano <46522867+JuanMorenoToledano2019@users.noreply.github.com>
2021-03-11 23:50:05 +0100

    He añadido menu_main

--- app/src/androidTest/java/com/dam/mynicesstart/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dam/mynicesstart/ExampleInstrumentedTest.java
@@ -2,12 +2,19 @@ package com.dam.mynicesstart;
 
 import android.content.Context;
 
+import androidx.test.espresso.ViewAction;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +24,35 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            SplashActivity.class);
+//    @Test
+//    public void useAppContext() {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//        assertEquals("com.dam.mynicesstart", appContext.getPackageName());
+//    }
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.dam.mynicesstart", appContext.getPackageName());
+    public void activityLaunch() {
+        onView(withId(R.id.button1)).perform(click());
+//        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+//        onView(withId(R.id.button_second)).perform(click());
+//        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
     }
+
+//    @Test
+//    public void textInputOutput() {
+//        onView(withId(R.id.editText_main)).perform(
+//                typeText("This is a test."));
+//        onView(withId(R.id.button_main)).perform(click());
+//        onView(withId(R.id.text_message)).check(
+//                matches(withText("This is a test.")));
+//    }
+
+
+
+
 }
