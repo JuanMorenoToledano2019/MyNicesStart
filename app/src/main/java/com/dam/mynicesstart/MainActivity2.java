e784f0900d847878cb077c44b965e850709ecd16
Juan Moreno Toledano <46522867+JuanMorenoToledano2019@users.noreply.github.com>
2021-03-11 23:49:46 +0100

    He añadido menu_main

--- app/src/main/java/com/dam/mynicesstart/MainActivity2.java
+++ app/src/main/java/com/dam/mynicesstart/MainActivity2.java
@@ -3,14 +3,18 @@ package com.dam.mynicesstart;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -22,8 +26,6 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        ivImagen2 = findViewById(R.id.ivImagen2);
-        btnBoton3 = findViewById(R.id.btnBoton3);
         Glide.with(this)
                 .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
 //                .apply(new RequestOptions()
@@ -32,6 +34,43 @@ public class MainActivity2 extends AppCompatActivity {
                 .transition(DrawableTransitionOptions.withCrossFade(1000))
 //                )
                 .into(ivImagen2);
+
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+
+        //click event en el  FAB
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity2.this, "FAB Clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //click event en el Hamburguer menu
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity2.this, "Menu clicked!", Toast.LENGTH_SHORT).show();
+//                sheetBehavior = BottomSheetBehavior.from(sheet);
+            }
+
+
+        });
+
+        //click event en el Bottom bar menu item
+        /*bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bottom_app_bar_menu_bookmark:
+                        Toast.makeText(MainActivity2.this, "More clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.bottom_app_bar_menu_search:
+                        Toast.makeText(MainActivity.this, "Search clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });*/
 
     }
 
